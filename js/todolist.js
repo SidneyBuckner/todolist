45211fcb987c4d12d5815878265ad78dce712daa
sidneybuckner <sidney.buckner2@gmail.com>
2018-05-23 14:35:53 -0500

    refectoring functions that got very messy

--- js/todolist.js
+++ js/todolist.js
@@ -7,25 +7,38 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
   //handle input
   if (input === "list"){
-    tasks.forEach(function(todo, i) {
-      alert( i + ": " + todo);
-    });
+    listTasks();
   }else if (input === "new") {
-    //ask for a new task
-  var newtask = prompt("Enter your new task:")
-    //add task to array
-    tasks.push(newtask);
-    alert("Your new task has been added!");
+    addToTasks();
   } else if (input === "delete") {
     //ask for index of the task to be deleted
-    var index = prompt("Are you sure??? Please provide the Index of the task you want to delete.");
-    //remove the task
-    tasks.splice(index, 1);
-    alert("Done, your task has been deleted.");
+    delTasks();
   }
   //ask again for new input
   input = prompt("What would you like to do?");
 }
 alert("OK, quitting app. Goodbye!");
+
+//Refectoring below:
+
+function listTasks() {
+  tasks.forEach(function(todo, i) {
+    alert( i + ": " + todo);
+  });
+}
+function addToTasks() {
+  //ask for a new task
+var newtask = prompt("Enter your new task:")
+  //add task to array
+  tasks.push(newtask);
+  alert("Your new task has been added!");
+}
+
+function delTasks() {
+  var index = prompt("Are you sure??? Please provide the Index of the task you want to delete.");
+  //remove the task
+  tasks.splice(index, 1);
+  alert("Done, your task has been deleted.");
+}
 
 }, 500);
